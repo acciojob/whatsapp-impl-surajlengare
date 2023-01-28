f09a70c2e69d61dcc659096faf2b9ec87d41f8c6
surajlengare <suraj.lengare@rsllearning.com>
2023-01-28 18:28:17 +0530

    Solution 2

--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -17,13 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("whatsapp")
-public class WhatsappController {
-
+public class WhatsappController
+{
     //Autowire will not work in this case, no need to change this and add autowire
     WhatsappService whatsappService = new WhatsappService();
 
     @PostMapping("/add-user")
-    public String createUser(String name, String mobile) throws Exception {
+    public String createUser( String name,  String mobile) throws Exception {
         //If the mobile number exists in database, throw "User already exists" exception
         //Otherwise, create the user and return "SUCCESS"
 
@@ -31,7 +31,7 @@ public class WhatsappController {
     }
 
     @PostMapping("/add-group")
-    public Group createGroup(List<User> users){
+    public Group createGroup( List<User> users){
         // The list contains at least 2 users where the first user is the admin. A group has exactly one admin.
         // If there are only 2 users, the group is a personal chat and the group name should be kept as the name of the second user(other than admin)
         // If there are 2+ users, the name of group should be "Group count". For example, the name of first group would be "Group 1", second would be "Group 2" and so on.
@@ -45,7 +45,7 @@ public class WhatsappController {
     }
 
     @PostMapping("/add-message")
-    public int createMessage(String content){
+    public int createMessage( String content){
         // The 'i^th' created message has message id 'i'.
         // Return the message id.
 
@@ -53,7 +53,7 @@ public class WhatsappController {
     }
 
     @PutMapping("/send-message")
-    public int sendMessage(Message message, User sender, Group group) throws Exception{
+    public int sendMessage( Message message,  User sender,  Group group) throws Exception{
         //Throw "Group does not exist" if the mentioned group does not exist
         //Throw "You are not allowed to send message" if the sender is not a member of the group
         //If the message is sent successfully, return the final number of messages in that group.
@@ -61,7 +61,7 @@ public class WhatsappController {
         return whatsappService.sendMessage(message, sender, group);
     }
     @PutMapping("/change-admin")
-    public String changeAdmin(User approver, User user, Group group) throws Exception{
+    public String changeAdmin( User approver,  User user, Group group) throws Exception{
         //Throw "Group does not exist" if the mentioned group does not exist
         //Throw "Approver does not have rights" if the approver is not the current admin of the group
         //Throw "User is not a participant" if the user is not a part of the group
@@ -70,7 +70,7 @@ public class WhatsappController {
         return whatsappService.changeAdmin(approver, user, group);
     }
 
-    @DeleteMapping("/remove-user")
+    /*@DeleteMapping("/remove-user")
     public int removeUser(User user) throws Exception{
         //This is a bonus problem and does not contains any marks
         //A user belongs to exactly one group
@@ -89,5 +89,5 @@ public class WhatsappController {
         // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
 
         return whatsappService.findMessage(start, end, K);
-    }
+    }*/
 }
--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -1,2 +1,47 @@
-package com.driver;public class WhatsappService {
+package com.driver;
+
+import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class WhatsappService
+{
+    WhatsappRepository whatsappRepository = new WhatsappRepository();
+
+    // 1)
+    public String createUser(String name, String mobile) throws Exception {
+           return whatsappRepository.createUser(name, mobile);
+
+    }
+
+    // 2)
+    public Group createGroup(List<User> users)
+    {
+        return whatsappRepository.createGroup(users);
+    }
+
+    // 3)
+    public int createMessage(String content)
+    {
+        return whatsappRepository.createMessage(content);
+    }
+
+    // 4)
+    public int sendMessage(Message message, User sender, Group group) throws Exception {
+
+           return whatsappRepository.sendMessage(message, sender, group);
+
+    }
+
+    // 5)
+    public String changeAdmin(User approver, User user, Group group) throws Exception
+    {
+
+             return whatsappRepository.changeAdmin(approver, user, group);
+
+    }
+
+
 }
